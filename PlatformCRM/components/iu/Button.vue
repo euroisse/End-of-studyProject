c19a6cbea22fd0a46593e1dce1b03b59ef4f6614
euroisse <euroissetsopkeng@gmail.com>
2025-04-17 12:58:00 +0100

    modified my ui component

--- PlatformCRM/components/iu/Button.vue
+++ PlatformCRM/components/iu/Button.vue
@@ -2,7 +2,7 @@
     <div>
     <button
       :class="[
-        'px-4 py-2 rounded-lg shadow-md transition-all',
+        'px-4 py-2 rounded-lg  transition-all',
         customClass,
       ]"
     >
