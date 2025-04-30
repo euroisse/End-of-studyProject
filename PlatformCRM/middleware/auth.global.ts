d48d06838bc4e2c30b019b38ba6f9714b328881e
euroisse <euroissetsopkeng@gmail.com>
2025-04-30 17:13:12 +0100

    adding my midleware auth

---
+++ PlatformCRM/middleware/auth.global.ts
@@ -0,0 +1,10 @@
+export default defineNuxtRouteMiddleware((to, from)=>{
+    // skip middleware on server
+    if (import.meta.server) return
+    const userInfos = localStorage.getItem('user');
+    if (!userInfos){
+        if(to.path !== '/'){
+            return navigateTo('/')
+        }
+    }
+})
