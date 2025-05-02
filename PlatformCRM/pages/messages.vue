823f96e0bd67fed4d6f72ee3bf808665a7ba1bc9
euroisse <euroissetsopkeng@gmail.com>
2025-05-02 12:32:32 +0100

    removing unnecessary components

--- PlatformCRM/pages/messages.vue
+++ PlatformCRM/pages/messages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="isClient" class="min-h-screen bg-gray-50 flex container">
+  <div v-if="isClient" class="min-h-screen bg-gray-50 flex">
     <MessagesConversationSidebar
       :conversations="conversations"
       @select="selectConversation"
@@ -70,7 +70,6 @@ import { ref, computed, onMounted, nextTick } from "vue";
 import { format } from "date-fns";
 import { fr } from "date-fns/locale";
 import type { Conversation, Message } from "@/types";
-import { useIsRole } from "#imports";
 
 definePageMeta({ layout: "admin" });
 
