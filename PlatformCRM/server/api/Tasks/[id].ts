e6d098a04b61f254d77eba765457b5b9cf0d01a8
euroisse <euroissetsopkeng@gmail.com>
2025-05-12 12:37:33 +0100

    modifying my api taskid

--- PlatformCRM/server/api/Tasks/[id].ts
+++ PlatformCRM/server/api/Tasks/[id].ts
@@ -1,13 +1,20 @@
+
 import prisma from "~/server/database";
 
 export default defineEventHandler(async (event) => {
   const taskId = parseInt(event.context.params?.id as string);
-  const body = await readBody(event)
+  const body = await readBody(event);
 
   const updated = await prisma.tasks.update({
     where: { id: taskId },
-    data: { status: body.status },
-  })
+    data: {
+      title: body.title,
+      description: body.description,
+      status: body.status,
+      priority: body.priority,
+      effort: body.effort ?? null,
+    },
+  });
 
-  return updated
-})
+  return updated;
+});
