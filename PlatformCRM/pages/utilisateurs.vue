67733d8e87a7746ed9cfd9402ea4c5a2d4b9fdb9
euroisse <euroissetsopkeng@gmail.com>
2025-05-28 17:25:09 +0100

    add my users page

---
+++ PlatformCRM/pages/utilisateurs.vue
@@ -0,0 +1,10 @@
+<template>
+  <div v-if="isAdmin" class="min-h-screen bg-gray-50 flex"></div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: "admin",
+});
+const { isAdmin } = useIsRole();
+</script>
