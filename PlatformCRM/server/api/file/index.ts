08259c1121a943f45c848cf0e062edccd7d7ccf2
euroisse <euroissetsopkeng@gmail.com>
2025-05-28 12:26:50 +0100

    updating my invoicePlayload

---
+++ PlatformCRM/server/api/file/index.ts
@@ -0,0 +1,30 @@
+
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+export default defineEventHandler(async (event) => {
+  
+  const invoiceNumber = event.context.params?.invoiceNumber;
+
+  if (!invoiceNumber) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Numéro de facture manquant.',
+    });
+  }
+
+  const filename = `facture_${invoiceNumber}.pdf`;
+  const filePath = path.join(process.cwd(), 'invoices', filename);
+
+  try {
+    const buffer = await readFile(filePath);
+
+    
+    setHeader(event, 'Content-Disposition', `attachment; filename="${filename}"`);
+    setHeader(event, 'Content-Type', 'application/pdf');
+
+    return buffer;
+  } catch (error: any) {
+    console.error(`Erreur lors de la lecture du fichier PDF ${filename}:`, error);
+  }
+});
