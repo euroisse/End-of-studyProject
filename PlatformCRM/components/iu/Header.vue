1dc86963650e77e5f5271b483799f647118c09ea
euroisse <euroissetsopkeng@gmail.com>
2025-06-11 21:53:06 +0100

    add condition of isloggin in button createAccount

--- PlatformCRM/components/iu/Header.vue
+++ PlatformCRM/components/iu/Header.vue
@@ -36,7 +36,7 @@
               @click="$emit('open-login')"
             />
             <Button
-              v-if="isAdmin"
+              v-if="isAdmin && !isLoggedIn"
               content=" Créer un compte"
               customClass="bg-indigo-500 hover:bg-indigo-800 text-white px-4 py-2 rounded-lg shadow-md transition-all whitespace-nowrap cursor-pointer !rounded-button"
               @click="$emit('open-register')"
